fa8635a6825cfde1bf0fb14520d209f27c2e111d
Lior Avraham <lioravraham7@gmail.com>
2025-06-21 09:39:03 +0300

    Initial Commit

---
+++ MoviesAppTests/MoviesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppTests.swift
+//  MoviesAppTests
+//
+//  Created by Student17 on 21/06/2025.
+//
+
+import Testing
+@testable import MoviesApp
+
+struct MoviesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
