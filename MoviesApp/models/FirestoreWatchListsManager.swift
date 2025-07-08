8cfdbaf462585be2476af24ce73fd586e54b9455
Lior Avraham <lioravraham7@gmail.com>
2025-07-08 12:27:05 +0300

    FirestoreWatchListsManager - completed

--- MoviesApp/models/FirestoreWatchListsManager.swift
+++ MoviesApp/models/FirestoreWatchListsManager.swift
@@ -10,7 +10,8 @@ import FirebaseFirestore
 
 // MARK: - FirestoreWatchListsManager protocols
 protocol FirestoreWatchListWriteDelegate: AnyObject {
-    func didAddMovieToWatchlistSuccessfully()
+    func didAddMovieToWatchlistSuccessfully(_ movie: MovieDetailsDTO)
+    func didFindMovieAlreadyInWatchlist(_ movie: MovieDetailsDTO)
     func didFailToAddMovieToWatchlist(error: Error)
 }
 
@@ -19,41 +20,166 @@ protocol FirestoreWatchListReadDelegate: AnyObject {
     func didFailToFetchWatchlist(error: Error)
 }
 
+protocol FirestoreWatchListDeleteDelete: AnyObject {
+    func didDeleteMovieFromWatchlistSuccessfully(movieID: Int)
+    func didFailToDeleteMovieFromWatchlist(error: Error)
+}
+
 // MARK: - FirestoreWatchListsManager class
 class FirestoreWatchListsManager {
     weak var writeDelegate: FirestoreWatchListWriteDelegate?
     weak var readDelegate: FirestoreWatchListReadDelegate?
+    weak var deleteDelegate: FirestoreWatchListDeleteDelete?
     
     private let db = Firestore.firestore() // reference to the Cloud firestore (the database)
     private let firebaseAuthManager = FirebaseAuthManager()
     
-    func addMovieToWatchList(movie: MovieDetailsDTO) {
-        // Ensure that the user is authenticated and get the current user's UID
-        guard let userID = firebaseAuthManager.getCurrentUser()?.uid else {
-            print("FirestoreWatchListManager: The user didn't authenticate")
-            return
-        }
-        
-        // Prepare the movie data to be saved in Firestore
-        let movieData: [String: Any] = [
-            Constants.FirestoreDB.MovieData.id: movie.id,
-            Constants.FirestoreDB.MovieData.originalTitle: movie.original_title,
-            Constants.FirestoreDB.MovieData.overview: movie.overview,
-            Constants.FirestoreDB.MovieData.posterPath: movie.poster_path,
-            Constants.FirestoreDB.MovieData.releaseDate: movie.release_date,
-            Constants.FirestoreDB.MovieData.runtime: movie.runtime,
-            Constants.FirestoreDB.MovieData.voteAverage: movie.vote_average,
-            
-            // Convert the genres array of objects into an array of dictionaries
-            Constants.FirestoreDB.MovieData.genres: movie.genres.map {
-                [Constants.FirestoreDB.MovieData.genreID: $0.id,
-                 Constants.FirestoreDB.MovieData.genreName: $0.name]
+    // MARK: - addMovieToWatchList()
+    func addMovieToWatchList(movie: MovieDetailsDTO) async {
+        Task {
+            do {
+                // Ensure that the user is authenticated and get the current user's UID
+                guard let userID = firebaseAuthManager.getCurrentUser()?.uid else {
+                    print("FirestoreWatchListManager: The user didn't authenticate")
+                    return
+                }
+                
+                // Define the Firestore document path: watchlists/{userID}/movies/{movieID}
+                let movieDocRef = db.collection(Constants.FirestoreDB.watchlists)
+                    .document(userID)
+                    .collection(Constants.FirestoreDB.movies)
+                    .document(String(movie.id))
+                
+                // Check if the movie already exist
+                let documentSnapshot = try await movieDocRef.getDocument()
+                if documentSnapshot.exists {
+                    writeDelegate?.didFindMovieAlreadyInWatchlist(movie)
+                    return
+                }
+                
+                // Prepare the movie data to be saved in Firestore
+                let movieData: [String: Any] = [
+                    Constants.FirestoreDB.MovieData.id: movie.id,
+                    Constants.FirestoreDB.MovieData.originalTitle: movie.original_title,
+                    Constants.FirestoreDB.MovieData.overview: movie.overview,
+                    Constants.FirestoreDB.MovieData.posterPath: movie.poster_path,
+                    Constants.FirestoreDB.MovieData.releaseDate: movie.release_date,
+                    Constants.FirestoreDB.MovieData.runtime: movie.runtime,
+                    Constants.FirestoreDB.MovieData.voteAverage: movie.vote_average,
+                    
+                    // Convert the genres array of objects into an array of dictionaries
+                    Constants.FirestoreDB.MovieData.genres: movie.genres.map {
+                        [Constants.FirestoreDB.MovieData.genreID: $0.id,
+                         Constants.FirestoreDB.MovieData.genreName: $0.name]
+                    }
+                ]
+                
+                // Save the movie to Firestore
+                try await movieDocRef.setData(movieData)
+                writeDelegate?.didAddMovieToWatchlistSuccessfully(movie)
+                
+            } catch {
+                // Handles errors that occourred during the process
+                writeDelegate?.didFailToAddMovieToWatchlist(error: error)
             }
-        ]
-        
-        // Define the Firestore path: watchlists/{userID}/movies/{movieID}
-        
+            // end Task
+        }
+        // end function
+    }
+    
+    // MARK: - fetchWatchList()
+    func fetchWatchList() {
+        Task {
             
-        
+            do {
+                // Ensure that the user is authenticated and get the current user's UID
+                guard let userID = firebaseAuthManager.getCurrentUser()?.uid else {
+                    print("FirestoreWatchListManager: The user didn't authenticate")
+                    return
+                }
+                
+                // Define the Firestore reference path: atchlists/{userID}/movies and get al documents from it
+                let snapshot = try await db.collection(Constants.FirestoreDB.watchlists)
+                    .document(userID)
+                    .collection(Constants.FirestoreDB.movies)
+                    .getDocuments()
+                
+                // Map documents to MovieDetailsDTO array
+                let movies: [MovieDetailsDTO] = snapshot
+                    .documents // return array of all ducuments that were in the collection [QueryDocumentSnapshot]
+                    .compactMap { doc in
+                        // Iterate on each doc and try to convert it to MovieDetailsDTO
+                        let data = doc.data() // data is the content of the dofcument in [String: Any] format
+                        
+                        guard let id = data[Constants.FirestoreDB.MovieData.id] as? Int,
+                              let original_title = data[Constants.FirestoreDB.MovieData.originalTitle] as? String,
+                              let overview = data[Constants.FirestoreDB.MovieData.overview] as? String,
+                              let poster_path = data[Constants.FirestoreDB.MovieData.posterPath] as? String,
+                              let release_date = data[Constants.FirestoreDB.MovieData.releaseDate] as? String,
+                              let runtime = data[Constants.FirestoreDB.MovieData.runtime] as? Int,
+                              let vote_average = data[Constants.FirestoreDB.MovieData.voteAverage] as? Double,
+                              let genresArray = data[Constants.FirestoreDB.MovieData.genres] as? [[String: Any]] else {
+                            print("FirestoreWatchListManager: The convert of the next data to MovieDetailsDTO failed: \(data)")
+                            return nil
+                        }
+                                
+                        let genres: [Genre] = genresArray.compactMap { genreDict in
+                            // Iterate on each genreDict - [String: Any] and try to convert it to Genre
+                            guard let id = genreDict[Constants.FirestoreDB.MovieData.genreID] as? Int,
+                                  let name = genreDict[Constants.FirestoreDB.MovieData.genreName] as? String else {
+                                print("FirestoreWatchListManager: The convert of the next genreDict to Genre failed: \(genreDict)")
+                                return nil
+                            }
+                            
+                            return Genre(id: id, name: name)
+                        }
+                        return MovieDetailsDTO(id: id,
+                                               original_title: original_title,
+                                               poster_path: poster_path,
+                                               overview: overview,
+                                               vote_average: vote_average,
+                                               release_date: release_date, runtime: runtime,
+                                               genres: genres)
+                    }
+                
+                // Notify delegate that the fetching executed successfully
+                readDelegate?.didFetchWatchlistSuccessfully(movies)
+                
+                
+            } catch {
+                // Handles errors that occourred during the process
+                readDelegate?.didFailToFetchWatchlist(error: error)
+            }
+            // end task
+        }
+        // end function
+    }
+    
+    // MARK: - deleteMovieFromWatchlist
+    func deleteMovieFromWatchlist(movieID: Int) {
+        Task {
+            do {
+                // Ensure that the user is authenticated and get the current user's UID
+                guard let userID = firebaseAuthManager.getCurrentUser()?.uid else {
+                    print("FirestoreWatchListManager: The user didn't authenticate")
+                    return
+                }
+                
+                // Define the Firestore document path: watchlists/{userID}/movies/{movieID}
+                let movieDocRef = db.collection(Constants.FirestoreDB.watchlists)
+                    .document(userID)
+                    .collection(Constants.FirestoreDB.movies)
+                    .document(String(movieID))
+                
+                // Try to delete the document
+                try await movieDocRef.delete()
+                deleteDelegate?.didDeleteMovieFromWatchlistSuccessfully(movieID: movieID)
+                
+            } catch {
+                deleteDelegate?.didFailToDeleteMovieFromWatchlist(error: error)
+            }
+            // end Task
+        }
+        // end function
     }
 }
